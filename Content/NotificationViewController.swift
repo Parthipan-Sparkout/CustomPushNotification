905957a87ac1f80eebabcb006da4031ba596c6ae
Parthipan <>
2023-04-12 19:07:23 +0530

    custom push notification work

---
+++ Content/NotificationViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NotificationViewController.swift
+//  Content
+//
+//  Created by Sparkout on 12/04/23.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
